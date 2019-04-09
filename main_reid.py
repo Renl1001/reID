57b7dc1192a3f677e68a8bfc66412dec23550536
任力 <renl1001@qq.com>
2019-04-09 14:39:08 +0800

    update softmax

--- main_reid.py
+++ main_reid.py
@@ -141,7 +141,7 @@ def train(**kwargs):
     print('Best rank-1 {:.1%}, achived at epoch {}'.format(best_rank1, best_epoch))
 
 def train_collate_fn(batch):
-    imgs, pids, _, _, = batch
+    imgs, pids, _, _, = zip(*batch)
     pids = torch.tensor(pids, dtype=torch.int64)
     return torch.stack(imgs, dim=0), pids
 
